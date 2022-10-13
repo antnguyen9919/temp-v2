3d0e387813f393816c15ebc84b22917b7fe3ebb4
An T. Nguyen <an.nguyen@tum.de>
2022-10-13 16:20:21 +0200

    final

--- src/pages/api/webhooks-test/index.js
+++ src/pages/api/webhooks-test/index.js
@@ -1,64 +1,34 @@
 import { db } from 'src/configs/firebase-admin'
 export default function (req, res) {
   console.log('Started')
-  if (req.method === 'POST') {
-    const {
-      receiver,
-      status,
+
+  const { dashboardId, evalMatches, orgId, panelId, ruleId, ruleName, ruleUrl, state, tags, title } = req.body
+
+  db.collection('alerts')
+    .add({
+      id: 'worked',
+      dashboardId,
+      evalMatches,
       orgId,
-      alerts,
-      groupLabels,
-      commonLabels,
-      commonAnnotations,
-      externalURL,
-      version,
-      groupKey,
-      truncatedAlerts,
-      title,
+      panelId,
+      ruleId,
+      ruleName,
+      ruleUrl,
       state,
-      message
-    } = req.body
-    db.collection('alerts')
-      .add({
-        id: 'worked',
-        receiver: receiver ? receiver : '',
-        status: status ? status : '',
-        orgId: orgId ? orgId : '',
-        alerts: alerts ? alerts : [],
-        groupLabels: groupLabels ? groupLabels : '',
-        commonLabels: commonLabels ? commonLabels : '',
-        commonAnnotations: commonAnnotations ? commonAnnotations : '',
-        externalURL: externalURL ? externalURL : '',
-        version: version ? version : '',
-        groupKey: groupKey ? groupKey : '',
-        truncatedAlerts: truncatedAlerts ? truncatedAlerts : '',
-        title: title ? title : '',
-        state: state ? state : '',
-        message: message ? message : ''
-      })
-      .then(res => {
-        if (res) {
-          console.log(res)
-        }
-      })
-      .catch(error => {
-        if (error) {
-          console.log(error.message)
-          return res.json({ error: error.message })
-        }
-      })
-  } else {
-    db.collection('alerts')
-      .add({ message: 'No body' })
-      .then(res => {
-        if (res) console.log('Ok boys')
-      })
-      .catch(e => {
-        if (e) {
-          console.log(e.message)
-          return res.json({ error: e.message })
-        }
-      })
-  }
+      tags,
+      title
+    })
+    .then(res => {
+      if (res) {
+        console.log(res)
+      }
+    })
+    .catch(error => {
+      if (error) {
+        console.log(error.message)
+        return res.json({ error: error.message })
+      }
+    })
+
   return res.status(200).json({ message: 'OK' })
 }
