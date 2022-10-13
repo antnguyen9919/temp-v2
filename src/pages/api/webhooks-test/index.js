65c276ba4b7f0f9525f39e60b59f7b7664504d6d
An T. Nguyen <an.nguyen@tum.de>
2022-10-13 16:35:21 +0200

    xa

--- src/pages/api/webhooks-test/index.js
+++ src/pages/api/webhooks-test/index.js
@@ -1,38 +1,43 @@
 import { db } from 'src/configs/firebase-admin'
 export default function (req, res) {
   console.log('Started')
-
-  const { dashboardId, evalMatches, orgId, panelId, ruleId, ruleName, ruleUrl, state, tags, title } = req.body
+  let response
+  let received = 0
 
   if (req.method !== 'POST') {
     db.collection('alerts').add({ name: 'Get requests' })
-    return res.status(200).json({ message: 'Wrong method but still recorded in Firestore' })
+    response = res.status(200).json({ message: 'Wrong method but still recorded in Firestore' })
+  } else {
+    const { dashboardId, evalMatches, orgId, panelId, ruleId, ruleName, ruleUrl, state, tags, title } = req.body
+    if (dashboardId || evalMatches || orgId || panelId || ruleId || ruleName || ruleUrl || state || tags || title)
+      received = 1
+    db.collection('alerts')
+      .add({
+        id: 'worked',
+        dashboardId: dashboardId ? dashboardId : '',
+        evalMatches: evalMatches ? evalMatches : '',
+        orgId: orgId ? orgId : '',
+        panelId: panelId ? panelId : '',
+        ruleId: ruleId ? ruleId : '',
+        ruleName: ruleName ? ruleName : '',
+        ruleUrl: ruleUrl ? ruleUrl : '',
+        state: state ? state : '',
+        tags: tags ? tags : '',
+        title: title ? title : ''
+      })
+      .then(res => {
+        if (res) {
+          console.log(res)
+          response = res.status(200).json({ message: 'Wrong method but still recorded in Firestore', received })
+        }
+      })
+      .catch(error => {
+        if (error) {
+          console.log(error.message)
+          response = res.json({ error: error.message })
+        }
+      })
   }
-  db.collection('alerts')
-    .add({
-      id: 'worked',
-      dashboardId,
-      evalMatches,
-      orgId,
-      panelId,
-      ruleId,
-      ruleName,
-      ruleUrl,
-      state,
-      tags,
-      title
-    })
-    .then(res => {
-      if (res) {
-        console.log(res)
-      }
-    })
-    .catch(error => {
-      if (error) {
-        console.log(error.message)
-        return res.json({ error: error.message })
-      }
-    })
 
-  return res.status(200).json({ message: 'OK' })
+  return response
 }
