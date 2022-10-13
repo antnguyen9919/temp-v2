c6b8b6a2f5f8d8b84e0dd8ac61ecc8341341375d
An T. Nguyen <an.nguyen@tum.de>
2022-10-13 15:37:38 +0200

    wh firestore

--- src/pages/api/webhooks-test/index.js
+++ src/pages/api/webhooks-test/index.js
@@ -1,4 +1,6 @@
 import { db } from 'src/configs/firebase-admin'
 export default function (req, res) {
+  const data = req.body
+  if (data) db.collection('alerts').add(data)
   return res.status(200).json({ message: 'Hello' })
 }
