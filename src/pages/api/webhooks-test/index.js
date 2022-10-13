2cfe365d342ad13c94b426b5a01a9e5018c9a8e2
An T. Nguyen <an.nguyen@tum.de>
2022-10-13 15:50:45 +0200

    sax

--- src/pages/api/webhooks-test/index.js
+++ src/pages/api/webhooks-test/index.js
@@ -1,6 +1,43 @@
 import { db } from 'src/configs/firebase-admin'
 export default function (req, res) {
-  const data = req.body
-  if (data) db.collection('alerts').add(data)
-  return res.status(200).json({ message: 'Hello' })
+  if (req.method === 'POST') {
+    const {
+      receiver,
+      status,
+      orgId,
+      alerts,
+      groupLabels,
+      commonLabels,
+      commonAnnotations,
+      externalURL,
+      version,
+      groupKey,
+      truncatedAlerts,
+      title,
+      state,
+      message
+    } = req.body
+    db.collection('alerts')
+      .add({
+        receiver: receiver ? receiver : '',
+        status: status ? status : '',
+        orgId: orgId ? orgId : '',
+        alerts: alerts ? alerts : [],
+        groupLabels: groupLabels ? groupLabels : '',
+        commonLabels: commonLabels ? commonLabels : '',
+        commonAnnotations: commonAnnotations ? commonAnnotations : '',
+        externalURL: externalURL ? externalURL : '',
+        version: version ? version : '',
+        groupKey: groupKey ? groupKey : '',
+        truncatedAlerts: truncatedAlerts ? truncatedAlerts : '',
+        title: title ? title : '',
+        state: state ? state : '',
+        message: message ? message : ''
+      })
+      .then(res => console.log(res))
+    return res.status(200).json({ message: 'Hello' })
+  } else {
+    db.collection('alerts').add({ message: 'No body' })
+    return res.status(200).json({ message: 'nothing happened' })
+  }
 }
